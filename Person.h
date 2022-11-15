69408a461d32a1100d5608f8b65b84c279f5e7a7
nirpr <nirpr@mta.ac.il>
2022-11-15 12:41:08 +0200

    added friends arr and signture of methods.

--- Person.h
+++ Person.h
@@ -6,7 +6,7 @@ Notes:
 Everything in note is need to set/finish 
 things only for first setup right now
 TODO:
-0. forward declartion and complete signatures of method inside ()    (partly completed)
+0. forward declartion and complete signatures of method inside ()    (partly completed, need to figure out addStatus)
 1. Constructors
 2. add setters/getters
 3. writing methods functions
@@ -22,21 +22,24 @@ public:
 	/*
 	Constructors (destroy copy make)
 	*/
-	bool addFriend();
-	bool removeFriend();
-	bool searchFriend();
-	bool addPage();
-	bool showMyStatus();
-	bool addStatus(); // self status
+	bool addFriend(Person* newFriend); // we can update the to objects with a pointer to the friend
+	bool removeFriend(Person* dFriend);
+	bool searchFriend(char* fName);
+	bool addPage(Page* newPage);
+	bool showMyStatus(const Status& myStatus); // const by ref so we could not change the status and save space. 
+	bool addStatus(); // self status, we need to think about how to hande it with different status types.
 private:
 	char* name;
 	int birthDay;
 	Status** myStatus;// =nullptr to set in c'tor
 	Status** FriendLatestStatus;// = nullptr to set in c'tor
 	Page** InterestInPages; //= nullptr to set in c'tor
+	Person** friends; // arr of pointers to persons friends, set to nullptr
+	unsigned int logSizeFriends; // set to zero
 	unsigned int logSizeMyStatus; // set to zero
 	unsigned int logSizeFriendsStatus; // set to zero
 	unsigned int logSizeInterestInPages; // set to zero
+	unsigned int phySizeFriends; // set to zero
 	unsigned int phySizeMyStatus; // set to zero
 	unsigned int phySizeFriendsStatus; // set to zero
 	unsigned int phySizeInterestInPages; // set to zero
